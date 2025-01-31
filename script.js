a20637d47344937bb40d658a422bdbb31909b664
kartik132000 <sujeetkumarsen37@gmail.com>
2025-01-31 20:53:33 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+        if (input.value !== '') {
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && index > 0 && input.value === '') {
+            inputs[index - 1].focus();
+        }
+    });
+});
+
+inputs[0].focus();
